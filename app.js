ad025c11814f53457d6e4b22ba549b98b1b7dba0
Zunnun <zunnunjuraev211@gmail.com>
2021-12-02 12:13:41 +0300

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const numberOfFilms = +prompt("сколько фильмов вы уже просмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+let a = prompt("Один из последных просмотренных фильмов?", "");
+let b = prompt("На сколько оцените его?", "");
+let c = prompt("Один из последных просмотренных фильмов?", "");
+let d = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
