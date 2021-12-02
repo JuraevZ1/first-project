f644bdddcaee69163d2b0167ae38a79ac9ade6fc
Zunnun <zunnunjuraev211@gmail.com>
2021-12-02 21:41:21 +0300

    add functions

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 "use strict";
 
-let numberWatchingFilms = +prompt("сколько фильмов вы уже просмотрели?");
-
+let numberWatchingFilms;
+function start() {
+  numberWatchingFilms = +prompt("сколько фильмов вы уже просмотрели?", "");
+  while (
+    numberWatchingFilms == "" ||
+    numberWatchingFilms == null ||
+    isNaN(numberWatchingFilms)
+  ) {
+    numberWatchingFilms = +prompt("сколько фильмов вы уже просмотрели?", "");
+  }
+}
+start();
 const personalMovieDB = {
   count: numberWatchingFilms,
   movies: {},
@@ -9,17 +19,18 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Один из последных просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
-  if (a !== null && b !== null && a !== "" && b !== "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    i--;
-    console.log("error");
+function showMyDB(obj) {
+  if (!obj) {
+    console.log(personalMovieDB);
   }
 }
+showMyDB(personalMovieDB.privat);
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`vash lyubimiy janr ${i}`, "");
+  }
+}
+writeYourGenres();
 // with while
 // let i = 0;
 // while (i < 2) {
@@ -48,15 +59,30 @@ for (let i = 0; i < 2; i++) {
 //   }
 //   i++;
 // } while (i < 2);
-
-if (personalMovieDB.count < 10) {
-  console.log("Посмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-  console.log("вы киноман");
-} else {
-  console.log("error from count");
+function rememberMovie() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последных просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
+    if (a !== null && b !== null && a !== "" && b !== "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      i--;
+      console.log("error");
+    }
+  }
 }
+rememberMovie();
 
-console.log(personalMovieDB);
+function detectPersonLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Посмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("вы киноман");
+  } else {
+    console.log("error from count");
+  }
+}
+detectPersonLevel();
